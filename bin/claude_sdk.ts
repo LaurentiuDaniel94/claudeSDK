82553aac373c47742db7d0d8c5bc0a12ee0e26b8
LauricaFaraFrica <laurentiuginghina@outlook.com>
2024-11-17 11:12:00 +0200

    feat: test

--- bin/claude_sdk.ts
+++ bin/claude_sdk.ts
@@ -2,7 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { ClaudeSdkStack } from '../lib/claude_sdk-stack';
-
+//test
 const app = new cdk.App();
 new ClaudeSdkStack(app, 'ClaudeSdkStack', {
 });
